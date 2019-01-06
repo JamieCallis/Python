096d85eedb099f9febf618c1921af8ecd8412561
JamieCallis <jamiecallis2014@gmail.com>
2019-01-06 20:37:56 +0000

    try-react-project

---
+++ django_projects/try-reactjs/try-reactjs/build/precache-manifest.cf6c0bbfb45f1e014a7141b58d653ad8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc9f1d14c39169c19f5a",
+    "url": "/static/js/main.cc9f1d14.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "cc9f1d14c39169c19f5a",
+    "url": "/static/css/main.af0a9aff.chunk.css"
+  },
+  {
+    "revision": "56597dafd215f3c7558cb2e08bd5c616",
+    "url": "/index.html"
+  }
+];
