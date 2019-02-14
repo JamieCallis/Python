a395bddaddd3426b20f1f62accfcd88b8e699c61
JamieCallis <jamiecallis2014@gmail.com>
2019-02-14 13:52:09 +0000

    update

--- python_work/BGP/BGP.py
+++ python_work/BGP/BGP.py
@@ -1,7 +1,7 @@
 from State import *
-from socket import socketfrom 
-from sysu import agrv
+from sys import argv
 from time import sleep
+import socket
 
 class Transition:
     def idle(self):
@@ -34,23 +34,40 @@ class Idle(State, Transition):
         to initiate a TCP connection to the BGP pper (client mode),
         or listens for a new conenct from a peer (server mode).
     '''
-    def __init__(self, Context)
+    def __init__(self, Context):
         State.__init__(self, Context)
 
     def idle(self):
-        # only called when operating as a server
+        # only called when operating as a server - this is handled by main
         # initiate listening process
         # and transition to connect state when connection made
+        print "Currently in Idle state, transition to connect state"
+        self.CurrentContext.listen()
+        self.CurrentContext.setState("CONNECT")
 
     def connect(self):
         # only called when operating as a client
         # initiate connection process
         # and transition to connect state when connection made
+        print "Currently in Idle state, transition to connect state"
+        self.CurrentContext.make_connection()
+        self.CurrentContext.setState("CONNECT")
     
     def trigger(self):
         # close open socket and reset object
         # return true if succeed. False otherwise
-    
+        
+        # Close the open Socket
+        if(self.CurrentContext.server):
+            self.CurrentContext.connection.close()
+            #self.idle()
+            return True
+        else:
+            #self.CurrentContext.connection.close()
+            #self.CurrentContext.setState("IDLE")
+            return True
+
+
 class Connect(State, Transition):
     '''
         The conect state's role is to send out open essage,
@@ -64,23 +81,30 @@ class Connect(State, Transition):
         State.__init__(self, Context)
     
     def idle(self):
-        self.CurrentContext.setState("IDEL")
+        print "currently in connect state, transition to IDLE state"
+        self.CurrentContext.setState("IDLE")
         return True
     def connect(self):
+        print "Currently in connect state staying in connect state"
         self.CurrentContext.setState("CONNECT")
         return True
     def active(self):
+        print "Currently in connect state, transition to active state"
         self.CurrentContext.setState("ACTIVE")
         return True
     def open_sent(self):
         # send open command via existing connection
+        print "Currently in connect state, transition to open sent state"
         # and transition to open sent 
         self.CurrentContext.setState("OPENSENT")
         return True
 
     def trigger(self):
         # display address of the connecting system
+        print "Connection from: " + str(self.CurrentContext.addr)
         # and trigger open_sent method
+        self.open_sent()
+        
 
 class Active(State, Transition):
     '''
@@ -124,19 +148,24 @@ class OpenSent(State, Transition):
         State.__init__(self, Context)
     
     def idle(self):
+        print "Currently in opensent, transition to IDLE state"
         self.CurrentContext.setState("IDLE")
         return True
     def active(self):
+        print "Currently in opensent, transition to active state"
         self.CurrentContext.setState("ACTIVE")
         return True
     def open_confirm(self):
+        print "Currently in opensent, transition to open_confirm"
         # when open command received, transition to open confirm state
-        self.CurrentContext.SETsTATE("OPENCONFIRM")
+        self.CurrentContext.setState("OPENCONFIRM")
         return True
 
     def trigger(self):
         # display address of system open command was sent to and
+
         # trigger open_confirm method
+        self.open_confirm()
 
 class OpenConfirm(State, Transition):
     '''
@@ -151,17 +180,22 @@ class OpenConfirm(State, Transition):
         State.__init__(self, Context)
     
     def idle(self):
+        print "Currently in open confirm, transition to IDLE state"
         self.CurrentContext.setState("IDLE")
         return True
     def open_confirm(self):
+        print "Currently in open confirm, transition to OPENCONFIRM state"
         # state stays the same as OPENCONFIRM
+        self.CurrentContext.setState("OPENCONFIRM")
         return True
     def established(self):
+        print "Currently in open confirm, transition to ESTABLISHED state"
         # send and receive keepalive messages
         self.CurrentContext.setState("ESTABLISHED")
     
     def trigger(self):
         # trigger established method
+        self.established()
 
 class Established(State, Transition):
     '''
@@ -174,15 +208,22 @@ class Established(State, Transition):
         State.__init__(self, Context)
         
     def idle(self):
+        print "Currently in Established, transition to IDLE state"
         self.CurrentContext.setState("IDLE")
         return True
     def established(self):
+        print "Currently in Established, staying in Established"
         # state stays the same
         return True
     def trigger(self):
         # terminate demo by transitioning to idle
+        self.idle()
 
 class BGPPeer(StateContext, Transition):
+    connection = None
+    addr = None
+    # True = server, False = client
+    server = None
     def __init__(self):
         # add the available states
         self.availableStates["IDLE"] = Idle(self)
@@ -192,6 +233,8 @@ class BGPPeer(StateContext, Transition):
         self.availableStates["OPENCONFIRM"] = OpenConfirm(self)
         self.availableStates["ESTABLISHED"] = Established(self)
         self.setState("IDLE")
+        self.host = "127.0.0.1"
+        self.port = 5000
         
     def idle(self):
         return self.CurrentState.idle()
@@ -213,10 +256,23 @@ class BGPPeer(StateContext, Transition):
     
     def listen(self):
         ''' this method initiates a listen socket '''
-    
-    def make_connection(self):
-        ''' this method initiates an outbound connection '''
+        # server
+        self.server = True
+        s = socket.socket()
+        s.bind((self.host, self.port))
+        s.listen(1)
+        print "waiting for a connection"
+        # connection acceptance
+        self.connection, self.addr = s.accept() 
 
+    def make_connection(self):
+        # client
+        ''' this method initiates an outbound connection '''
+        print "making a connection"
+        self.server = False
+        s = socket.socket()
+        s.connect((self.host, self.port))
+        
 if __name__ == '__main__':
     if len(argv) < 2:
         print "Error: too few arguments"
