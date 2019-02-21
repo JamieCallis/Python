585fdf60ad1c45c45dc5b9ebc15953b0c0564363
JamieCallis <jamiecallis2014@gmail.com>
2019-02-21 17:47:24 +0000

    CW2 skeleton code

--- python_work/CW2/16112008_client.py
+++ python_work/CW2/16112008_client.py
@@ -7,7 +7,7 @@ class State:
 class StateContext:
     stateIndex = 0
     CurrentState = NoneavailableStates = []
-    availableStates = []
+    availableStates = [] # Issue by Coursework or done by purpose. 
 
     def setState(self, newstate):
         self.CurrentState = self.availableStates[newstate]
@@ -18,25 +18,124 @@ class StateContext:
 
 class Transition:
     def closed(self):
+        print "Error can't transition to closed!"
+        return False
+
+    def synSent(self):
+        print "Error can't transition to synSent!"
+        return False
+    
+    def established(self):
+        print "Error can't transition to established!"
+        return False
+    
+    def finWait_1(self):
+        print "Error can't transition to finwait_1!"
+        return False
+    
+    def finWait_2(self):
+        print "Error can't transition to finwait_2!"
+        return False
+  
+    def timedWait(self):
+        print "Error can't transition to timedWait!"
+        return False
+    
+    
+
+class Closed(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+
+    def closed(self):
         pass
 
     def synSent(self):
         pass
-    
+
+    def Trigger(self):
+        pass
+
+class SynSent(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+
+    def closed(self):
+        pass
+
     def established(self):
         pass
     
+    def Trigger(self):
+        pass
+
+class Established(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+
     def finWait_1(self):
         pass
-    
+
+    def Trigger(self):
+        pass
+
+class FinWait_1(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+
     def finWait_2(self):
         pass
-  
-    def TimedWait(self):
+
+    def Trigger(self):
+        pass
+
+class FinWait_2(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+
+    def timedWait(self):
+        pass
+
+    def Trigger(self):
+        pass
+
+class TimedWait(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+
+    def closed(self):
+        pass
+
+    def Trigger(self):
         pass
 
 class TCPSimulatorClient(StateContext, Transition):
-    pass
+    def __init__(self):
+        self.availableStates["CLOSED"] = Closed(self)
+        self.availableStates["SYNSENT"] = SynSent(self)
+        self.availableStates["ESTABLISHED"] = Established(self)
+        self.availableStates["FINWAIT-1"] = FinWait_1(self)
+        self.availableStates["FINWAIT-2"] = FinWait_2(self)
+        self.availableStates["TIMEDWAIT"] = TimedWait(self)
+        self.setState("CLOSED")
+    
+    def closed(self):
+        return self.CurrentState.closed()
+
+    def synSent(self):
+        return self.CurrentState.synSent()
+    
+    def established(self):
+        return self.CurrentState.established()
+    
+    def finWait_1(self):
+        return self.CurrentState.finWait_1()
+    
+    def finWait_2(self):
+        return self.CurrentState.finWait_2()
+  
+    def timedWait(self):
+        return self.CurrentState.timedWait()
 
 if __name__ == "__main__":
     pass
--- python_work/CW2/16112008_server.py
+++ python_work/CW2/16112008_server.py
@@ -20,29 +20,137 @@ class StateContext:
 
 class Transition:
     def closed(self):
-        pass
+        print "Error can't transition to closed!"
+        return False
     
+    def listen(self):
+        print "Error can't transition to listen!"
+        return False
+
+    def synRecvd(self):
+        print "Error can't transition to synRecvd!"
+        return False
+    
+    def established(self):
+        print "Error can't transition to established!"
+        return False
+    
+    def closeWait(self):
+        print "Error can't transition to closeWait!"
+        return False
+    
+    def lastAck(self):
+        print "Error can't transition to lastAck!"
+        return False
+    
+    def TimedWait(self):
+        print "Error can't transition to TimedWait!"
+        return False
+
+class Closed(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+    
+    def closed(self):
+        pass
+
     def listen(self):
         pass
 
+    def Trigger(self):
+        pass
+
+class Listen(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+
     def synRecvd(self):
         pass
-    
-    def established(Self):
+       
+    def Trigger(self):
         pass
+
+class SynRecvd(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+
+    def established(self):
+        pass
+
+    def Trigger(self):
+        pass
+
+class Established(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
     
     def closeWait(self):
         pass
-    
+
+    def Trigger(self):
+        pass
+
+class CloseWait(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+
     def lastAck(self):
         pass
-    
-    def TimedWait(self):
+
+    def Trigger(self):
         pass
 
+class LastAck(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+
+    def closed(self):
+        pass
+
+    def Trigger(self):
+        pass
+# is timed wait even needed?
+class TimedWait(State, Transition):
+    def __init__(self, Context):
+        State.__init__(self, Context)
+        
+    def Trigger(self):
+        pass
 
 class TCPSimulatorServer(StateContext, Transition):
-    pass
+    def __init__(self):
+        # add the available states
+        self.availableStates["CLOSED"] = Closed(self)
+        self.availableStates["LISTEN"] = Listen(self)
+        self.availableStates["SYNRECVD"] = SynRecvd(self)
+        self.availableStates["ESTABLISHED"] = Established(self)
+        self.availableStates["CLOSEDWAIT"] = CloseWait(self)
+        self.availableStates["LASTACT"] = LastAck(self)
+        self.availableStates["TIMEDWAIT"] = TimedWait(self)
+        self.setState("CLOSED")
+        self.host = "127.0.0.1"
+        self.port = 5000
+    
+    def closed(self):
+        return self.CurrentState.closed()
+    
+    def listen(self):
+        return self.CurrentState.listen()
+
+    def synRecvd(self):
+        return self.CurrentState.synRecvd()
+    
+    def established(self):
+        return self.CurrentState.established()
+    
+    def closeWait(self):
+        return self.CurrentState.closeWait()
+    
+    def lastAck(self):
+        return self.CurrentState.lastAck()
+    
+    def TimedWait(self):
+        return self.CurrentState.timedWait() 
 
 if __name__ == "__main__":
     pass
