aba8c23bdcbb5ebb96c7e8602c349b60867a2088
JamieCallis <jamiecallis2014@gmail.com>
2019-02-24 15:03:51 +0000

    chatper9
    Finished the NLP Python Book, chapter 10-12 are illreivant to future studies.

---
+++ NLP-Python/NLP-Deep-learning/NLP-Book/chapter9/gramaticalfeatures.py
@@ -0,0 +1,146 @@
+import nltk
+
+# feature structures
+kim = {'CAT': 'NP', 'ORTH': 'Kim', 'REF': 'k'}
+chase = {'CAT': 'V', 'ORTH': 'chased', 'REL': 'chase'}
+
+chase['AGT'] = 'sbj'
+chase['PAT'] = 'obj'
+
+'''
+    We want to "bind" the verb's agent role
+    to the subject and the patient role to
+    the object.
+
+    We can do this by assuming that NPs
+    immediately to the left and right of 
+    the verb are the subject and object
+    respectively.
+'''
+
+sent = "Kim chased Lee"
+tokens = sent.split()
+lee = {'CAT': 'NP', 'ORTH': 'Lee', 'REF': '1'}
+
+def lex2fs(word):
+    for fs in [kim, lee, chase]:
+        if fs['ORTH'] == word:
+            return fs
+
+subj, verb, obj = lex2fs(tokens[0]), lex2fs(tokens[1]), lex2fs(tokens[2])
+verb['AGT'] = subj['REF']
+verb['PAT'] = obj['REF']
+
+for k in ['ORTH', 'REL', 'AGT', 'PAT']:
+    print("%-5s => %s" % (k, verb[k]))
+
+'''
+    We can apply the same approach
+    to as many different verbs as we want 
+    too.
+
+    E.g. surprise
+'''
+
+surprise = {'CAT': 'V', 'ORTH': 'surprised', 'REL': 'surprise',
+           'SRC': 'sbj', 'EXP': 'obj'}
+
+
+'''
+Agreement Paradigm for English Regular Verbs
+                Singular        Plural
+1st person      I run           We run
+2nd person      you run         you run
+3rd person      he/she/it runs  they run
+
+We use "3" for 3rd person, "SG" for singular
+and "PL" for plural.
+'''
+
+grammar = nltk.CFG.fromstring("""
+    S       -> NP_SG VP_SG
+    S       -> NP_PL VP_PL
+    NP_SG   -> Det_SG N_SG
+    NP_PL   -> Det_PL N_PL
+    VP_SG   -> V_SG
+    VP_PL   -> V_PL
+
+    Det_SG  -> 'this'
+    Det_PL  -> 'these'
+    N_SG    -> 'dog'
+    N_PL    -> 'dogs'
+    V_SG    -> 'runs'
+    V_PL    -> 'run'
+""")
+
+# But using this approach would mean blowing
+# up the dataset by a factor of 6. In order
+# to cover all of the posabilities. 
+
+# Using Attributes and Constraints
+
+grammar1 = nltk.CFG.fromstring("""
+    N[NUM=pl]
+
+    DET[NUM=sg] ->  'this'
+    DET[NUM=pl] ->  'these'
+
+    N[NUM=sg]   ->  'dog'
+    N[NUM=pl]   ->  'dogs'
+    V[NUM=sg]   ->  'runs
+    V[NUM=pl]   ->  'run'
+""")
+
+# This is forced the English person context
+# rules. 
+
+# But we can take this a set up again using
+# variables.
+# forces the NP and VP to take the same rule.
+# Since you have to have both as singular, or plural.
+# In the event that the rule isn't met the result will be an fail.
+grammar2 = nltk.CFG.fromstring("""
+    S           ->  NP[NUM=?n]  VP[NUM=?n]
+    NP[NUM=?n]  ->  Det[NUM=?n] N[NUM=?n]
+    VP[NUM=?n]  ->  V[NUM=?n]
+""")
+
+'''
+    Features such as values like sg and pl. Are called
+    atomic.
+
+    We can also get boolean values such as 'auxiliary verbs'
+    such as can, may, will, do. 
+
+    The following classifies as true / false
+    V[TENSE=pres, +AUX] -> 'can'
+    V[TENSE=pres, +AUX] -> 'may'
+
+    V[TENSE=pres, -AUX] -> 'walks'
+    V[TENSE=pres, -AUX] -> 'likes'
+'''
+
+# Nested feature structures
+
+'''
+    In addition to atomic-valued features, features may
+    take values that a re themsleves feature structures.
+
+    We can create groups of categories such as:-
+
+    [POS = N           ]
+    [                  ]
+    [AGR = [PER = 3   ]]
+    [      [NUM = pl  ]]
+    [      [GND = fem ]]
+    
+    - Order is not specific.
+
+    Grouped together as AGR. We say that AGR has a complex value.
+
+    Depicts the structure, in a format known as an attribute 
+    value matrix (AVM).
+'''
+
+
+
---
+++ NLP-Python/NLP-Deep-learning/NLP-Book/chapter9/processingfeaturestructures.py
@@ -0,0 +1,174 @@
+# Processing feature structures
+
+import nltk
+from nltk import load_parser
+# in nltk we define feature structures as follows.
+fs1 = nltk.FeatStruct(PER=3, NUM='pl', GND='fem')
+
+
+# we can view the structures as a kind of dictionary.
+# such we can acess values by indexing in the ususal way.
+print fs1['GND']
+
+# adding to the feature structure
+fs1['CASE'] = 'acc'
+
+# We can also build more complex feature structures as follows.
+fs2 = nltk.FeatStruct(POS='N', ARG=fs1)
+print fs2
+print fs2['ARG']
+print fs2['ARG']['PER']
+
+# feature structures are general purpose structures
+# such we don't have to only use the for linguistic features.
+print(nltk.FeatStruct(NAME='Lee', TELNO='01 27 86 42 96', AGE=33))
+
+'''
+    We can think of feature structures as graphs
+    more specifically.
+
+    Directed acyclic graphs (DAGs)
+
+    Of which we would often thing of such graphs
+    in terms of paths through the graph. A feature path
+    is a sequence of arcs that can be followed from the root node.
+
+    Paths will be represneted as truples.
+
+    Another point to note is we can share sub-graph points
+    between different arcs. Siad to be equivalent.
+    
+    Such reducing the amount of repeat code.
+'''
+
+fs3 = nltk.FeatStruct("""[NAME='Lee', ADDRESS=(1)[NUMBER=74, STREET='rue Pascal'],
+                        SPOUSE=[NAME='Kim', ADDRESS->(1)]]""")
+print fs3
+
+# Subsumption and Unification
+
+'''
+    When combining feature sets they are
+    partical information objects. 
+
+    We can order features by how much
+    information they contain. 
+
+    E.g.
+    
+    a.		
+[NUMBER = 74]
+
+b.		
+[NUMBER = 74          ]
+[STREET = 'rue Pascal']
+
+c.		
+[NUMBER = 74          ]
+[STREET = 'rue Pascal']
+[CITY = 'Paris'       ]
+
+This ordering is called subsumption.
+
+b subsumes a, and c subsumes b. Such that
+if all the information in a feature set is
+contained within another featureset. 
+
+It is consumed by the featureset. Such
+to not repeat information.
+'''
+
+# Unification 
+
+fs1 = nltk.FeatStruct(NUMBER=74, STREET='rue Pascal')
+fs2 = nltk.FeatStruct(CITY='Paris')
+print fs1.unify(fs2)
+
+'''
+    Unification is the process of joining
+    two feature sets together. 
+
+    It create a Union between the two 
+    feature sets such as. 
+
+    FS0 u FS1 = FS1 u FS0
+
+    If we unify two feature structures which
+    stand in the subsumption relationship,
+    then the result of unification is the most
+    informative of the two.
+
+    Aka. The one with the largest amount
+    of data.
+
+    Unification process will fail if the
+    two feature structures share the same
+    path but the values are atomically distinct.
+'''
+
+# Looking at hw unification interacts with
+# structure sharing.
+
+fs0 = nltk.FeatStruct("""[NAME=Lee,
+                           ADDRESS=[NUMBER=74,
+                                    STREET='rue Pascal'],
+                           SPOUSE= [NAME=Kim,
+                                    ADDRESS=[NUMBER=74,
+                                             STREET='rue Pascal']]]""")
+
+print fs0
+
+# lets say we want to add a city feature for kim's address
+fs1 = nltk.FeatStruct("[SPOUSE = [ADDRESS = [CITY = Paris]]]")
+print fs1.unify(fs0)
+
+# can be noted that fs1 had to take the entire
+# fs0 feature structure.
+
+# if we create a new feature adding in 
+# address sharing
+fs2 = nltk.FeatStruct("""[NAME=Lee, ADDRESS=(1)[NUMBER=74, STREET='rue Pascal'],
+                           SPOUSE=[NAME=Kim, ADDRESS->(1)]]""")
+print fs1.unify(fs2)
+
+# we see that the address category adds city
+# as an attribute.
+
+# Since we can express features sets with variables
+fs1 = nltk.FeatStruct("[ADDRESS1=[NUMBER=74, STREET='rue Pascal']]")
+fs2 = nltk.FeatStruct("[ADDRESS1=?x, ADDRESS2=?x]")
+print fs2
+print fs2.unify(fs1) # shall build the relationship between address 2 and 1
+# giving them the attributes of number and street.
+
+
+# X-Bar Syntax - Heads Revisited
+
+'''
+    Is a method of abstracting out the
+    notion of 'phrasal level'. 
+
+    Normally represents three levels
+
+    such that if N represents the lexical
+    level, then N' represents the next
+    level up, sorresponding to the more
+    transitional category NOM.
+
+    While N'' represnts the phrasal level
+    corresponding to the category NP.
+
+    S           -> N[BAR=2] V[BAR=2]
+    N[BAR=2]    -> Det N[BAR=1]
+    N[BAR=1]    -> N[BAR=1] P[BAR=2]
+    N[BAR=1]    -> N[BAR=0] P[BAR=2]
+    N[BAR=1]    -> N[BAR=0]XS
+'''
+
+
+
+
+
+
+
+
